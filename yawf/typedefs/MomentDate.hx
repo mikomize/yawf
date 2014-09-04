24c492f644037a211c77c76f66ffd0c536bd2f7d
miko <milosz.kordecki@gmail.com>
2014-09-04 10:19:17 +0200

    moment date

---
+++ yawf/typedefs/MomentDate.hx
@@ -0,0 +1,12 @@
+
+package ;
+
+extern class MomentDate {
+	function new(timestamp:Int);
+	function unix():Int;
+	function tz(timezone:String):MomentDate;
+	function startOf(startOf:String):MomentDate;
+	function add(amount:Int, what:String):MomentDate;
+	function substract(amount:Int, what:String):MomentDate;
+	function format(f:String):String;
+}
