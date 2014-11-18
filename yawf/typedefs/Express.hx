cfa89059669953ba1db98289e98beeabd26d0c41
miko <milosz.kordecki@gmail.com>
2014-11-18 23:18:53 +0100

    zzz

--- yawf/typedefs/Express.hx
+++ yawf/typedefs/Express.hx
@@ -9,6 +9,7 @@ extern class Express
 	public function get(path:String, fn:ExpressHttpServerReq ->  ExpressHttpServerResp -> Void):Void;
 	public function post(path:String, fn:ExpressHttpServerReq ->  ExpressHttpServerResp -> Void):Void;
 	@:overload(function(d:Dynamic):Void{})
+	@:overload(function(fn:Dynamic -> ExpressHttpServerReq ->  ExpressHttpServerResp -> (Dynamic -> Void) -> Void):Void{})
 	public function use(?path:String, fn:Dynamic -> ExpressHttpServerReq ->  ExpressHttpServerResp -> (Dynamic -> Void) -> Void):Void;
 	public function listen(port:Int, ?host:String):Void;
 }
