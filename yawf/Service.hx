aaf2a58628af347a563de76c5043db9a730cbff7
miko <milosz.kordecki@gmail.com>
2015-07-13 17:33:15 +0200

    verbose

--- yawf/Service.hx
+++ yawf/Service.hx
@@ -19,16 +19,16 @@ class Service
 	@inject
 	public var logger:ILogger;
 
-	public function success(msg:Dynamic):Void {
+	public function success(msg:Dynamic, verbose:Bool = true):Void {
 		var res:Dynamic = getResponseData();
 		res.result = ObjectMapper.toPlainObject(msg);
-		sendResponse(res);
+		sendResponse(res, verbose);
 	}
 
 	public function error(msg:Dynamic):Void {
 		var res:Dynamic = getResponseData();
 		res.error = ObjectMapper.toPlainObject(msg);
-		sendResponse(res);
+		sendResponse(res, true);
 	}
 
 	private function getResponseData():Dynamic {
@@ -40,13 +40,14 @@ class Service
 	    return resData;
 	}
 
-	private function sendResponse(responseData:Dynamic) {
-		if (responseData.error == null) {
-			logger.verbose("OK#" + Node.stringify(responseData.result));
-		} else {
-			logger.verbose("ERROR#", Node.stringify(responseData.error));
-		} 
-		
+	private function sendResponse(responseData:Dynamic, verbose:Bool = false) {
+		if (verbose) {
+			if (responseData.error == null) {
+				logger.verbose("OK#" + Node.stringify(responseData.result));
+			} else {
+				logger.verbose("ERROR#", Node.stringify(responseData.error));
+			}
+		}
 	    requestData.res.json(200, responseData);
   	}	
 
