012516c45e1c1e617b9f3c55bb06ddacf90fefbe
mosowski <mateusz.osowski@tensquaregames.com>
2015-10-08 17:11:47 +0200

    Redis Set

---
+++ yawf/redis/RedisSet.hx
@@ -0,0 +1,65 @@
+
+package yawf.redis;
+
+import yawf.ObjectMapper;
+
+@:generic
+class RedisSet<T> extends RedisKey {
+
+	@inject
+	public var redis:RedisLayer;
+
+	private var data:T;
+
+	public function new(key:String) {
+		super(key);
+	}
+
+	public function add(members:Array<T>, callback:Dynamic->Int->Void):Void {
+		redis.client.sadd(key, untyped serializeArray(members), callback);
+	}
+
+	public function rem(members:Array<T>, callback:Dynamic->Int->Void):Void {
+		redis.client.srem(key, untyped serializeArray(members), callback);
+	}
+
+	public function members(callback:Dynamic->Array<T>->Void):Void {
+		redis.client.smembers(key, function(error:Dynamic, arrayOfStrings:Dynamic):Void {
+			callback(error, deserializeArray(arrayOfStrings));
+		});
+	}
+
+	public function getCardinality(callback:Dynamic->Int->Void) {
+		redis.client.scard(key, callback);
+	}
+
+	private function format(res:Array<Dynamic>):Array<Pair<T, Int>> {
+		var tmp:Array<Pair<T, Int>> = new Array<Pair<T, Int>>();
+		while (res.length != 0) {
+			var pair:Pair<T, Int> = new Pair<T, Int>(deserialize(cast(res.shift(), String)), Std.parseInt(res.shift()));
+			tmp.push(pair);
+		}
+		return tmp;
+	}
+
+	private function serializeArray(items:Array<T>):Array<String> {
+		return Lambda.array(Lambda.map(items, function(t:T):String {
+			return serialize(t);
+		}));
+	}
+
+	private function deserializeArray(items:Array<String>):Array<T> {
+		return Lambda.array(Lambda.map(items, function(s:String):T {
+			return deserialize(s);
+		}));
+	}
+
+	private function serialize(item:T):String {
+		return ObjectMapper.toJson(item);
+	}
+
+	private function deserialize(json:String):T {
+		return ObjectMapper.fromJsonUntyped(json, classInfo.getField("data").type);
+	}
+
+}
