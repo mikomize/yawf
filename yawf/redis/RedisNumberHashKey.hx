865dd79a56c0b35e986ddd234dfcdec031c46022
miko <milosz.kordecki@gmail.com>
2016-08-10 16:47:21 +0200

    get all dla redishashnumber

--- yawf/redis/RedisNumberHashKey.hx
+++ yawf/redis/RedisNumberHashKey.hx
@@ -20,4 +20,14 @@ class RedisNumberHashKey extends RedisKey {
 			cb(current);
 		});
 	}
+
+	public function getAll(cb:Map<String, Int>->Void) {
+		redis.client.hgetall(key, function (err:Dynamic, all:Dynamic) {
+			var res:Map<String, Int> = new Map<String, Int>();
+			for (field in Reflect.fields(all)) {
+					res.set(field, Reflect.field(all, field));
+			}
+			cb(res);
+		});
+	}
 }
